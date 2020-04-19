63a9eeb30d2e0e950e8d041c0919cf9ecd09ae1e
samir char <samirchar@msn.com>
2020-04-19 13:48:47 -0500

    added __init__.py and fixed subprocess call to corenlp server.

--- dataprep/stanford_corenlp/utils.py
+++ dataprep/stanford_corenlp/utils.py
@@ -8,11 +8,20 @@ from retry import retry
 class serverConnection:
     
     def __init__(self):
-        self.command = ["java", "-mx4g","-cp",'"*"', "edu.stanford.nlp.pipeline.StanfordCoreNLPServer", \
-                        "-preload","tokenize,ssplit,pos,lemma,parse,depparse",\
-                        "-status_port","9000","-port","9000","-timeout","15000"]
+        self.command = ['java',
+                        '-mx4g',
+                        '-cp',
+                        '*',
+                        'edu.stanford.nlp.pipeline.StanfordCoreNLPServer',
+                        '-preload',
+                        'tokenize,ssplit,pos,lemma,parse,depparse',
+                        '-status_port',
+                        '9000',
+                        '-port',
+                        '9000',
+                        '-timeout',
+                        '15000']
         self.cwd = os.path.dirname(__file__)
-    
     
     def start_server(self):
         self.process = subprocess.Popen(self.command,cwd = self.cwd)
